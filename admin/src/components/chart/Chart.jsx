4ceb1135826bd2ad7d4b41d2cc384a94fcd491f8
DeshanTD <tdeshan.lk@gmail.com>
2024-09-09 20:18:14 +0530

    fixed overflow in chart and made modifications

--- admin/src/components/chart/Chart.jsx
+++ admin/src/components/chart/Chart.jsx
@@ -1,13 +1,18 @@
 import './chart.css'
 import React from 'react';
 import { LineChart, Line, XAxis, Tooltip, CartesianGrid, ResponsiveContainer } from 'recharts';
-
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 export default function Chart({title, data, dataKey, grid}) {
+  const isMobile = useMediaQuery('(max-width:480px)');
+  const isTablet = useMediaQuery('(max-width:768px)');
+
+  const aspect = isMobile ? 16/9 : isTablet ? 2/1 : 4/1;
+  
   return (
     <div className='chart'>
       <h3 className="chartTitle">{title}</h3>
-      <ResponsiveContainer width="100%" aspect={4/1}> {/* if height is 4 units width is 1 unit*/}
+      <ResponsiveContainer width="100%" aspect={aspect}> {/* if height is 4 units width is 1 unit*/}
         <LineChart data={data}>
             <XAxis dataKey="name" stroke="#5550bd" />
             <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
